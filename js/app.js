a726ee5208850ba55d4ad968eda99cf73d912f23
Zowie Erickson <zowie.erickson@gmail.com>
2021-04-19 22:22:29 -0400

    JS alert notification completed, implemented traffic line graph

--- js/app.js
+++ js/app.js
@@ -1,1 +1,65 @@
-alert("hi");
+const alert = document.querySelector("#alert");
+const trafficCanvas = document.querySelector("#traffic-chart");
+
+alert.innerHTML = `
+<div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-banner-close">x</p>
+</div>
+`;
+
+alert.addEventListener("click", (e) => {
+  const element = e.target;
+  if (element.classList.contains("alert-banner-close")) {
+    alert.style.display = "none";
+  }
+});
+
+let trafficData = {
+  labels: [
+    "16-22",
+    "23-29",
+    "30-5",
+    "6-12",
+    "13-19",
+    "20-26",
+    "27-3",
+    "4-10",
+    "11-17",
+    "18-24",
+    "25-31",
+  ],
+  datasets: [
+    {
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+
+      hoverOffset: 4,
+      background: "green",
+      borderWidth: 4,
+      borderColor: "rgba(112, 104, 201, .5)",
+    },
+  ],
+};
+
+let trafficOptions = {
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0,
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+  type: "line",
+  data: trafficData,
+  options: trafficOptions,
+});
