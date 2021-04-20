9b575794c9ac6eeaf5f4bb127e94d5bb14108e8b
Zowie Erickson <zowie.erickson@gmail.com>
2021-04-20 13:44:45 -0400

    daily and mobile charts added in and styled

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const alert = document.querySelector("#alert");
 const trafficCanvas = document.querySelector("#traffic-chart");
+const dailyCanvas = document.querySelector("#daily-chart");
+const mobileCanvas = document.querySelector("#mobile-chart");
 
 alert.innerHTML = `
 <div class="alert-banner">
@@ -14,6 +16,8 @@ alert.addEventListener("click", (e) => {
     alert.style.display = "none";
   }
 });
+
+// LINE CHART ------------------------------------------
 
 let trafficData = {
   labels: [
@@ -34,7 +38,6 @@ let trafficData = {
       data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
 
       hoverOffset: 4,
-      background: "green",
       borderWidth: 4,
       borderColor: "rgba(112, 104, 201, .5)",
     },
@@ -42,6 +45,9 @@ let trafficData = {
 };
 
 let trafficOptions = {
+  backgroundColor: "rgba(112, 104, 201, .3)",
+  tension: 0.3,
+  fill: true,
   aspectRatio: 2.5,
   animation: {
     duration: 0,
@@ -62,4 +68,79 @@ let trafficChart = new Chart(trafficCanvas, {
   type: "line",
   data: trafficData,
   options: trafficOptions,
+});
+
+// BAR CHART --------------------------------------------
+
+let dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [
+    {
+      data: [75, 110, 170, 120, 230, 200, 100],
+    },
+  ],
+};
+
+let dailyOptions = {
+  backgroundColor: "rgba(112, 104, 201, 1)",
+  tension: 0.3,
+  fill: true,
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0,
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+  type: "bar",
+  data: dailyData,
+  options: dailyOptions,
+});
+
+// PIE CHART --------------------------------------------
+
+let mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [
+    {
+      data: [65, 20, 15],
+    },
+  ],
+};
+
+let mobileOptions = {
+  backgroundColor: [
+    "rgba(112, 104, 201, 1)",
+    "rgb(114, 197, 114)",
+    "rgba(44, 144, 221, .8)",
+  ],
+  hoverOffset: 4,
+  borderWidth: 0,
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0,
+  },
+  plugins: {
+    legend: {
+      display: true,
+      align: "center",
+      // position: "right",
+    },
+  },
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: "doughnut",
+  data: mobileData,
+  options: mobileOptions,
 });
